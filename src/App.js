041dcab980b010be0ba9d667b401d3df92cd38cf
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-01-20 23:32:30 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Apps</h1>
-    </div>
-  );
-}
+import Cards from './components/Cards/Cards'
+import CountryPicker from './components/CountryPicker/CountryPicker'
+import Charts from './components/Chart/Chart'
+
+import styles from './App.module.css'
+
+import {fetchData} from './api';
+
+ class App extends React.Component {
+   state={
+     data:{}
+   }
+
+  async componentDidMount(){
+    const fetchedData= await fetchData(); //calling function
+    
+    this.setState({
+      data: fetchedData
+    })
+  }
+
+  render() {
+    const {data} =this.state
+    return (
+      <div className={styles.container}>
+        <Cards data={data}/>
+        <CountryPicker />
+        <Charts />
+      </div>
+    )
+  }
+ }
 
 export default App;
