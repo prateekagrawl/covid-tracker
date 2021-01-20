9a204ed3aeea00e9edbf1d0a8f6d87f8be63fcc1
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-01-20 23:31:43 +0530

    update chart

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,27 @@
+import React, {useState,useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+import {Line, Bar} from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
+
+
+const Chart= () => {
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() =>{
+        const fetchAPI = async() =>{
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    });
+
+
+    return (
+        <div>
+           <h1>Chart</h1> 
+        </div>
+    )
+}
+
+export default Chart
